96629d88bbb4c53548223acc0bf5c50fb12ffc5b
WuZongzhen <103302166+WuZongzhen@users.noreply.github.com>
2024-02-06 13:41:10 +0800

    Update train_SSTFormer.sh

--- SSTFormer/train_SSTFormer.sh
+++ SSTFormer/train_SSTFormer.sh
@@ -1,2 +1,2 @@
-python tools/train.py configs/recognition/SSTFormer/SSTFormer.py --work-dir work_dirs/SSTFormer  --seed 0 --deterministic  
+CUDA_VISIBLE_DEVICES=0 python tools/train.py configs/recognition/SSTFormer/SSTFormer.py --work-dir work_dirs/SSTFormer  --seed 0 --deterministic  
 
